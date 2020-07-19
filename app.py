5a2f7798e39f9033f7c067030fee309e89b5363a
Matthew Miniat <60368690+Minitaurr@users.noreply.github.com>
2020-07-19 18:17:07 -0500

    copiedd project file for testing

---
+++ app.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict")
+def ml_app():
+    return render_template("ml.html")
+
+@app.route("/respiratory_diseases")
+def respitory():
+    return render_template("respiratory.html")
+
+@app.route("/external_causes")
+def external():
+    return render_template("external.html")
+
+@app.route("/nervous_system")
+def nervous():
+    return render_template("nervous.html")
+
+@app.route("/metabolic_disorders")
+def endocrine():
+    return render_template("endocrine.html")
+
+@app.route("/mental_disorders")
+def mental():
+    return render_template("mental.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
